52296d5d52ab559e86a13097951e6e14b850bbd4
Edwin-Zarco <64220994+Edwin-Zarco@users.noreply.github.com>
2023-02-27 22:58:16 +0000

    Create DaddyLiveHD Alphabetical Sorting.js

---
+++ DaddyLiveHD Alphabetical Sorting.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        DaddyLiveHD Alphabetical Sorting
+// @match       https://*.daddylivehd.sx/*
+// @version     1.0
+// @author      Edwin Zarco
+// @description Alphabetically sorts the channels in the channel grid
+// @license     AGPL-3.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const gridContainer = document.querySelector('.grid-container');
+    const gridItems = [...gridContainer.querySelectorAll('.grid-item')];
+    gridItems.sort((a, b) => a.textContent.localeCompare(b.textContent));
+    gridItems.forEach(item => gridContainer.appendChild(item));
+})();
